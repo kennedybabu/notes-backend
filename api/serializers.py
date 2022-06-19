38eab955816c02316c2c4cd94077df648f0ae8df
kennedybabu <kennedynjogu8@gmail.com>
2022-06-19 08:16:40 +0300

    create the serializer file

---
+++ api/serializers.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+from .models import Note
+from rest_framework.serializers import ModelSerializer
+
+
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+from .models import Note
+
 # Create your views here.
 
 
-
+@api_view([''])
 def getRoutes(request):
+    routes= [
 
-    return JsonResponse('API response', safe=False)
+    ]
+
+    return Response('API response')
+
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serializer = serializer
+    return Response('NOTES')
